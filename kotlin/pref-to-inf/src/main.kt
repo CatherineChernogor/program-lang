f070031f844b8474d1e7d22c65973418516b1d61
Catherine <43898150+CatherineChernogor@users.noreply.github.com>
2020-10-13 16:47:59 +0800

    fix pref2inf task

--- kotlin/pref-to-inf/src/main.kt
+++ kotlin/pref-to-inf/src/main.kt
@@ -10,56 +10,72 @@ fun isNumber(s: String): Boolean {
     return true
 }
 
+fun isOp(str: String): Boolean {
+    val ops = arrayOf("+", "-", "*", "/")
+    for (op in ops) {
+        if (op.equals(str)) return true
+    }
+    return false
+}
+
 fun pop(s: MutableList<String>): String {
 
-    var result = s.first()
-    s.removeAt(0)
+    var result = s.get(s.lastIndex)
+    s.removeAt(s.lastIndex)
     return result
+}
+
+fun sum(first: String, second: String, sign: String): String {
+
+    if (isOp(sign)) return "(" + first + sign + second + ")"
+    else return "(" + sign + first + second + ")"
 }
 
 fun main(args: Array<String>) {
 
     print("Please input expression: ")
     var answer: String? = readLine()
-    val ops = arrayOf("+", "-", "*", "/")
+
     if (!answer.isNullOrEmpty()) {
 
-        while (answer?.get(answer.length - 1)?.equals(' ')!!) {
+        while (answer?.get(answer.length - 1)?.equals(' ')!!) {//remove extra spaces
             answer = answer?.substring(0, answer.lastIndex)
         }
-        
+
         var parts = ArrayList<String>(answer?.split(' '))
         val stack = mutableListOf<String>()
 
-        for (part in parts.reversed()) {
+        for (part in parts) {
 
-            if (isNumber(part)) {
-                stack.add(0, part)
-            } else if (part in ops) {
+            if (isOp(part)) {
+                stack.add(part)
+            } else if (isNumber(part)) {//if cur is number
 
-                if (stack.size > 1) {
-                    var first = pop(stack)
-                    var second = pop(stack)
-                    var str = "(" + first + part + second + ")"
-                    stack.add(0, str)
-                } else {
-                    println("Not enough arguments")
-                    return
+                if (isOp(stack.get(stack.lastIndex))) {//and previous is op
+                    stack.add(part)
+                } else {//and previous is also number
+                    var str = sum(pop(stack), part, pop(stack))
+                    stack.add(str)
                 }
             } else {
                 println("Wrong expression. There're not recognized symbol ")
                 return
             }
         }
-
-        if (stack.size > 1) {
-            println("Not enough operators to unpack")
-        } else if (stack.isEmpty()) {
-            println("Stack is empty")
-        } else {
-            println("Result: ${stack.removeAt(stack.lastIndex)}")
+        while (stack.size > 2) {
+            var s = pop(stack)
+            var f = pop(stack)
+            var str = sum(f, s, pop(stack))
+            stack.add(str)
         }
+
+
+        if (stack.size > 1) println("Not enough operators or numbers to unpack")
+        else if (stack.isEmpty()) println("Stack is empty")
+        else println("Result: ${stack.removeAt(stack.lastIndex)}")
+
     } else {
         println("Expression is empty")
     }
 }
+
